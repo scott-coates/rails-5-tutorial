fa591c510a833ecc2f7930a15bf17d56d50abc8c
scott coates <scoarescoare@gmail.com>
2017-03-19 12:22:35 -0500

    login and signup

--- tutorial/config/routes.rb
+++ tutorial/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
 
   resources :microposts
   resources :users
+  post '/signup', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static_pages#home'
 end
--- tutorial/test/models/user_test.rb
+++ tutorial/test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new name: 'Foo', email: 'foo@bar.com', password:'foo',password_confirmation:'foo'
+  end
+
+  test "should_be_valid" do
+    assert @user.valid?
+  end
 end
